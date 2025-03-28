859140669cbcc624ee1b32d72eb0def9344182e2
Lukevz <luke@blueskylabs.io>
2025-03-28 14:23:27 -0400

    updated title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'Lumos Notes'
+export const SITE_DESCRIPTION = 'This is my evolving space—a place where I explore ideas, document thoughts, and refine what I’m learning over time.'
