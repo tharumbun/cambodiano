e1f8c07419c37bec4d78f34143a952b03db08c97
kokitree <hello@kokitree.com>
2025-05-04 15:30:13 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://cambodiano.com',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
         shikiConfig: {
